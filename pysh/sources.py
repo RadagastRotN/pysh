53f652bbccaa47fbbc832fb8b4253160404420e2
Zbigniew Kaleta <zkaleta@agh.edu.pl>
2023-06-28 13:55:30 +0200

    Added bz2_cat Refactoring

--- pysh/sources.py
+++ pysh/sources.py
@@ -1,3 +1,5 @@
+import bz2
+
 from pysh import wc, Flags
 from pysh.file_utils import _to_absolute
 from pysh.generator import make_source, generator
@@ -18,6 +20,12 @@ def cat(filename, with_len=False):
     return generator(inner(), len_=_len)
 
 
-@make_source
-def cat_list(l):
-    yield from l
+@make_source(5)
+def cat_list(lst):
+    yield from lst
+
+
+def bz2_cat(filename):
+    with bz2.open(filename, 'rt') as infile:
+        for line in infile:
+            yield line[:-1]
